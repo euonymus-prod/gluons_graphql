2990ca36cf734e50133075c825a5b981ae33beaf
euonymus <euonymus0220@gmail.com>
2018-12-14 15:45:52 +0900

    tutorial model experiment

--- gluons/graphql_api/models.py
+++ gluons/graphql_api/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    url = models.URLField()
+    description = models.TextField(blank=True)
+    # created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    link = models.ForeignKey('graphql_api.Link', related_name='votes', on_delete=models.CASCADE)
