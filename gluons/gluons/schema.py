d794ab1b187a503924e605e74db5a29cf2c71e75
euonymus <euonymus0220@gmail.com>
2018-12-18 12:21:35 +0900

    quark_types

--- gluons/gluons/schema.py
+++ gluons/gluons/schema.py
@@ -2,18 +2,21 @@ import graphene
 import graphql_jwt
 
 import graphql_api.schema_quark
+import graphql_api.schema_quark_type
 import users.schema
 
 
 class Query(
         users.schema.Query,
         graphql_api.schema_quark.Query,
+        graphql_api.schema_quark_type.Query,
         graphene.ObjectType):
     pass
 
 class Mutation(
         users.schema.Mutation,
         graphql_api.schema_quark.Mutation,
+        graphql_api.schema_quark_type.Mutation,
         graphene.ObjectType,
 ):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
