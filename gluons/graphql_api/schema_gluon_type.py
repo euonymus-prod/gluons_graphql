706a53cbc536a1fd24408db11a06e02e301ef81e
euonymus <euonymus0220@gmail.com>
2019-01-21 01:06:18 +0900

    this might be the best practice

--- gluons/graphql_api/schema_gluon_type.py
+++ gluons/graphql_api/schema_gluon_type.py
@@ -2,13 +2,40 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from users.schema import UserType
-from graphql_api.models import GluonType
+from graphql_api.models import Gluon, GluonType
 from graphql import GraphQLError
 from django.db.models import Q
+
+class GluonModel2Type(DjangoObjectType):
+    class Meta:
+        model = Gluon
 
 class GluonTypeType(DjangoObjectType):
     class Meta:
         model = GluonType
+
+    gluons = graphene.List(
+        GluonModel2Type,
+        first=graphene.Int(),
+        skip=graphene.Int(),
+        orderBy=graphene.String(),
+    )
+
+    def resolve_gluons(self, info, first=None, skip=None, **kwargs):
+        # The value sent with the search parameter will be in the args variable
+        orderBy = kwargs.get("orderBy", None)
+        if orderBy:
+            qs = Gluon.objects.order_by(orderBy).reverse()
+        else:
+            qs = Gluon.objects.all()
+
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
 
 class Query(graphene.ObjectType):
