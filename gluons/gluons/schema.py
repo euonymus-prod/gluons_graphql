a9cfa083cbfca4e5c3c8eba76fea1b86b4a92583
euonymus <euonymus0220@gmail.com>
2019-02-07 13:44:16 +0900

    refactoring

--- gluons/gluons/schema.py
+++ gluons/gluons/schema.py
@@ -1,40 +1,19 @@
 import graphene
 import graphql_jwt
 
-import graphql_api.schema_quark
-# import graphql_api.schema_gluon
-# import graphql_api.schema_quark_type
-# import graphql_api.schema_gluon_type
-# import graphql_api.schema_quark_property
-# import graphql_api.schema_qtype_property
-# import graphql_api.schema_qproperty_gtype
-# import graphql_api.schema_qproperty_type
+import graphql_api.schema
 import users.schema
 
 
 class Query(
         users.schema.Query,
-        graphql_api.schema_quark.Query,
-        # graphql_api.schema_gluon.Query,
-        # graphql_api.schema_quark_type.Query,
-        # graphql_api.schema_gluon_type.Query,
-        # graphql_api.schema_quark_property.Query,
-        # graphql_api.schema_qtype_property.Query,
-        # graphql_api.schema_qproperty_gtype.Query,
-        # graphql_api.schema_qproperty_type.Query,
+        graphql_api.schema.Query,
         graphene.ObjectType):
     pass
 
 class Mutation(
         users.schema.Mutation,
-        graphql_api.schema_quark.Mutation,
-        # graphql_api.schema_gluon.Mutation,
-        # graphql_api.schema_quark_type.Mutation,
-        # graphql_api.schema_gluon_type.Mutation,
-        # graphql_api.schema_quark_property.Mutation,
-        # graphql_api.schema_qtype_property.Mutation,
-        # graphql_api.schema_qproperty_gtype.Mutation,
-        # graphql_api.schema_qproperty_type.Mutation,
+        graphql_api.schema.Mutation,
         graphene.ObjectType,
 ):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
